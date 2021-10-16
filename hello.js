2a96feff652b7fb020d404f9d474697c20f74f51
BohuaFu <54387777+BohuaFu@users.noreply.github.com>
2021-10-16 23:30:36 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!111", {
+  const response = new Response("Hello World!, My Name is BohuaFu, Nice to meet you...", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
